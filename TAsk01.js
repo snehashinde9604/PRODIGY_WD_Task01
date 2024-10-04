d53580207d6cad22445add2507a12f37bcae3a3a
snehashinde9604 <159521005+snehashinde9604@users.noreply.github.com>
2024-10-04 13:12:57 +0530

    Create TAsk01.js

---
+++ TAsk01.js
@@ -0,0 +1,11 @@
+# JavaScrpit
+
+// Function to change navbar background on scroll
+window.onscroll = function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+};
